9b694bb6c6781512c0f266be2bb0344f7d6a934c
ᄃΉЯIƧƬӨPΉΣЯ (BᄂΣᄃKMΛПП-)DЯΣΉΣЯ <root@schniggie.de>
2022-11-20 11:05:13 +0100

    Added docker volume binds for $PWD/resolvers.txt and $PWD/subdomains.txt

--- docker-subdomains.sh
+++ docker-subdomains.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-	docker run --rm -it subdomains.sh
+        docker run --rm -it subdomains.sh
 elif [ $1 = "build" ]; then
-	docker build . -t subdomains.sh
+        docker build . -t subdomains.sh
 elif [ $1 = "destroy" ]; then
-	docker rmi -f subdomains.sh:latest
+        docker rmi -f subdomains.sh:latest
 else
-	docker run --rm -it subdomains.sh $@
+        docker run --rm -it -v $PWD/resolvers.txt:/home/subdomains/resolvers.txt -v $PWD/subdomains.txt:/home/subdomains/subdomains.txt subdomains.sh $@
 fi
